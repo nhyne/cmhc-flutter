63275faab339b682e0a6c0ca5dbd2b2be822f156
johnsona <adamjohnson35@gmail.com>
2018-04-09 20:04:29 -0400

    load the steps propoerly

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,19 @@ class ProtocolsState extends State<CmhcProtocolsList> {
   Map _protocols;
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  Future<List> _getSteps(String protocolId) async {
+    var httpClient = new HttpClient();
+    httpClient.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+
+    var url = "https://cmhc-protocols.org/api/protocols/" + protocolId + "/steps";
+
+    var request = await httpClient.getUrl(Uri.parse(url));
+    var response = await request.close();
+    var responseBody = await response.transform(utf8.decoder).join();
+    Map data = json.decode(responseBody);
+    return data['data'];
+  }
+
   Future<List> _getProtocols() async {
     var httpClient = new HttpClient();
     httpClient.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
@@ -36,28 +49,82 @@ class ProtocolsState extends State<CmhcProtocolsList> {
     var response = await request.close();
     var responseBody = await response.transform(utf8.decoder).join();
     Map data = json.decode(responseBody);
-    // print(data['data']);
     return data['data'];
   }
 
-  Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
+  void _selectTile(Map data) {
+    var futureBuilder = new FutureBuilder(
+      future: _getSteps(data['id']),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return new ListTile(
+              title: new Text('loading...', style: _biggerFont)
+            );
+          default:
+            if (snapshot.hasError)
+              return new Text('Error: ${snapshot.error}');
+            else
+              return createStepListView(context, snapshot);
+        }
+      }
+    );
+
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Steps'),  
+            ),
+            body: futureBuilder,
+          );
+        }
+      ),
+    );
+  }
+
+  Widget createProtocolListView(BuildContext context, AsyncSnapshot snapshot) {
     List values = snapshot.data;
     return new ListView.builder(
       itemCount: values.length,
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (BuildContext context, int index) {
         // Add a one-pixel-high divider widget before each row in theListView.
-        return _buildRow(values[index]);
+        return _buildProtocolRow(values[index]);
       }
     );
   }
 
-  Widget _buildRow(Map data) {
+  Widget createStepListView(BuildContext context, AsyncSnapshot snapshot) {
+    List values = snapshot.data;
+    return new ListView.builder(
+      itemCount: values.length,
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext context, int index) {
+        // Add a one-pixel-high divider widget before each row in theListView.
+        return _buildStepRow(values[index]);
+      }
+    );
+  }
+
+  Widget _buildStepRow(Map data) {
+    return new ListTile(
+      title: new Text(
+        data['attributes']['information'],
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildProtocolRow(Map data) {
     return new ListTile(
       title: new Text(
         data['attributes']['title'],
         style: _biggerFont,
       ),
+      onTap: () { _selectTile(data); },
     );
   }
 
@@ -76,7 +143,7 @@ class ProtocolsState extends State<CmhcProtocolsList> {
             if (snapshot.hasError)
               return new Text('Error: ${snapshot.error}');
             else
-              return createListView(context, snapshot);
+              return createProtocolListView(context, snapshot);
         }
       }
     );
